fb812b4e378c4ba01736d21b4a7d41b2cd1264fd
mamontovss <mamontov.s@digitald.pro>
2022-06-29 00:20:27 +0600

    Создана функция создания голосования

--- contracts/voting.sol
+++ contracts/voting.sol
@@ -1,2 +1,15 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
+
+contract voting{
+address public owner;
+bool votingStatus; // информация о статусе голосования создано ли оно или нет
+    constructor (){
+        owner = msg.sender; // идентификация создателя контракта
+    }
+  
+function createVoting() public {
+    require (msg.sender == owner);
+    votingStatus = true;
+}
+}
